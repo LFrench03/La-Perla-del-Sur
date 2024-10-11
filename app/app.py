87e9996cdc1312bc9b0e26e144d3827b75350fd5
LFrench03 <luisernestoserrras@gmail.com>
2024-10-10 22:39:18 -0400

    init_1

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 #Dependences
 import streamlit as st
+from json import load
 import folium
-import csv
 from streamlit_folium import st_folium
 import pandas as pd
 from folium import Map, GeoJson, GeoJsonPopup, GeoJsonTooltip, Choropleth, LayerControl
@@ -10,14 +10,27 @@ import numpy as np
 import plotly_express as px
 import plotly.graph_objects as go
 
-
 #Function to convert each dataframe to a downloadable csv
 @st.cache_data
 def convert_df(df):
     return df.to_csv().encode("utf-8")
 
-#Header
+#Function to skip initial space on csv's files
+def skipinitalspace_csv(path:str):
+    from csv import reader, writer
+    data = []
+    with open(path, "r") as f:
+        reader = reader(f, skipinitialspace=True)
+        for i in reader:
+            data.append(i)
+    with open(path, 'w') as f:
+        writer = writer(f)
+        writer.writerows(data)
+
+#Page configuration
 st.set_page_config(page_title="La Perla del Sur", page_icon="app/img/perla.jpeg", layout="centered")
+
+#Header
 st.markdown('<h1 align="center"><img src="https://readme-typing-svg.herokuapp.com?font=Righteous&size=35&center=true&vCenter=true&width=500&height=60&duration=4000&lines=La+Perla+del+Sur+⚪️;" /> </h1>',unsafe_allow_html=True)
 with st.container(border=True):
     st.image(image="app/img/cienfuegos2.jpeg", use_column_width=True)
@@ -25,11 +38,75 @@ with st.container(border=True):
     st.divider()
     st.markdown("",unsafe_allow_html=True)
 
-# Dialect to CSV's
-csv.register_dialect("my_dialect",
-                     delimiter = ",",
-                     skipinitialspace = True,
-                     quoting = csv.QUOTE_ALL)
+# CSV's paths
+files = ["app/data/csv/ Matrícula inicial educacion superior por curso.csv",
+         "app/data/csv/ Población residente según edad laboral por zonas urbana y rural (a) (cálculos al 31 de diciembre de 2019).csv",
+         "app/data/csv/ Salario medio mensual en entidades estatales por municipios.csv",
+         "app/data/csv/Graduados educacion superior por cursos.csv",
+         "app/data/csv/Movimiento migratorio interno por sexos y provincias.csv",
+         "app/data/csv/Salario medio mensual en entidades estatales y mixtas por provincias.csv",
+         "app/data/csv/Saldos migratorios y tasa de saldo migratorio total por provincias.csv",
+         "app/data/csv/Saldos migratorios y tasas de migracion interna y externa por provincias-.csv",
+         "app/data/csv/Saldos migratorios y tasas de migración interna y externa, por municipios.csv",
+         "app/form/data/La-Perla-del-Sur-Form.csv"]
+
+
+# I-Migratory movments:
+df_mm = pd.read_csv(files[4]) #Read csv
+def migratory_movements(df: pd.core.frame.DataFrame, type:str="prov") -> list[pd.core.frame.DataFrame]:
+    if type == "prov":
+        year2012, year2013, year2014, year2015, year2016, year2017, year2018, year2019, year2020, year2021, year2022 = 0,0,0,0,0,0,0,0,0,0,0
+        dfs = [year2012, year2013, year2014, year2015, year2016, year2017, year2018, year2019, year2020, year2021, year2022]
+        i = 1
+        for index in range(len(dfs)):
+            j = i + 49
+            dfs[index] = df.iloc[i:j:3,:]
+            dfs[index].set_index("PROCEDENCIA/DESTINO", inplace=True)
+            dfs[index].index.name=None
+            i = j + 3
+    elif type == "mun":
+        year2019, year2020, year2021, year2022 = 0,0,0,0
+        dfs = [year2019, year2020, year2021, year2022]
+        i = 1
+        for index in range(len(dfs)):
+            j = i + 8
+            dfs[index] = df.iloc[i:j,:]
+            dfs[index].set_index("AÑOS", inplace=True)
+            dfs[index].index.name=None
+            i = j + 1        
+    return dfs
+df_mm = migratory_movements(df_mm)
+
+#Load Geojson
+with open("app/data/geojsons/cuba.geojson") as json_file:
+    data = load(json_file)
+
+
+lista_prov, data2012, data2013, data2014, data2015, data2016, data2017, data2018, data2019, data2020, data2021, data2022 = sorted(["Artemisa", "Camagüey","Ciego de Ávila","Cienfuegos","Granma","La Habana","Matanzas","Sancti Spíritus","Las Tunas","Holguín","Santiago de Cuba","Isla de la Juventud","Villa Clara","Guantánamo","Pinar del Río","Mayabeque"]), {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+ids, years, values = ["art","cam","cav","cfg", "gra", "gtm", "hol" , "ijv" ,"lha","ltu" ,"mat","may","pri","ssp","stg","vcl"], [x for x in range(2012,2023)], [data2012,data2013,data2014,data2015,data2016,data2017,data2018,data2019,data2020,data2021,data2022] 
+
+for df, year in list(zip(values, [x for x in range(2012, 2023)])):
+    for i in lista_prov:
+        df[i] = {}
+        for id, prov in list(zip(ids, lista_prov)):            
+            value = df_mm[years.index(year)].loc[i,prov]
+            df[i][id] = int(value) if value != "-" else 0
+
+# Movimientos migratorios intermunicipales 
+df_mun = migratory_movements(df, "mun")
+
+# Saldo & tasa de migracion interprovincial total
+df_smt = pd.read_csv(files[6])
+prov_order = list(df_smt.iloc[::13]["PROVINCIAS/AÑOS"])
+df_smt = migratory_movements(df_smt, 'etc')
+
+# Saldo & tasa de migracion interprovincial interno & externo
+skipinitalspace_csv(files[7])
+df_sm = pd.read_csv(files[7])
+df_sm = migratory_movements(df_sm, 'etc')
+
+
+
 
 
 
--- scripts/add_values_geojson.py
+++ scripts/add_values_geojson.py
@@ -1,31 +1,57 @@
 import json 
+import pandas as pd
 '''
-Script para agregar los valores que se mostrarian en el tooltip del mapa al 'data/geojsons/cuba.geojson'
+Script para agregar los valores que se mostrarian en el tooltip del mapa al geojson
 '''
-with open("data/geojsons/cuba.geojson") as json_file:
+with open("app/data/geojsons/cuba.geojson") as json_file:
     data = json.load(json_file)
-with open("inventario_ganado.json") as json_file2:
-    data2 = json.load(json_file2)
+path = "app/data/csv/Movimiento migratorio interno por sexos y provincias.csv"
 
-lista_prov = ["La Habana","Matanzas","Cienfuegos","Sancti Spiritus","Las Tunas","Holguin","Granma","Santiago de Cuba","Isla de la Juventud"
-              ,"Camaguey","Ciego de Avila","Villa Clara","Guantanamo","Pinar del Rio","Artemisa","Mayabeque"] #Provincias en el orden que aparece en la estructura del GeoJson
+def skipinitalspace_csv(path:str):
+    from csv import reader, writer
+    data = []
+    with open(path, "r") as f:
+        reader = reader(f, skipinitialspace=True)
+        for i in reader:
+            data.append(i)
+    with open(path, 'w') as f:
+        writer = writer(f)
+        writer.writerows(data)
+skipinitalspace_csv(path)
 
+df = pd.read_csv(path)
+def migratory_movements(df: pd.core.frame.DataFrame) -> list[pd.core.frame.DataFrame]:
+    year2012, year2013, year2014, year2015, year2016, year2017, year2018, year2019, year2020, year2021, year2022 = 0,0,0,0,0,0,0,0,0,0,0
+    dfs = [year2012, year2013, year2014, year2015, year2016, year2017, year2018, year2019, year2020, year2021, year2022]
+    i = 1
+    for index in range(len(dfs)):
+        j = i + 49
+        dfs[index] = df.iloc[i:j:3,:]
+        dfs[index].set_index("PROCEDENCIA/DESTINO", inplace=True)
+        dfs[index].index.name=None
+        i = j + 3
+    return dfs
 
-tierras=data2["Instituciones"]["Tenientes de tierras"]  #Luego "Por Provincias"
+df = migratory_movements(df)
+years = [x for x in range(2012,2023)] #Ordederd list of years
+provincias = list(df[0].columns)[1:]
+
+#Provincias en el orden que aparece en la estructura del GeoJson
+lista_prov = ["La Habana","Matanzas","Cienfuegos","Sancti Spíritus","Las Tunas","Holguín","Granma","Santiago de Cuba","Isla de la Juventud"
+              ,"Camagüey","Ciego de Ávila","Villa Clara","Guantánamo","Pinar del Río","Artemisa","Mayabeque"] 
+
 index = 0
-for i, j  in list(zip([x for x in range(16)], lista_prov)):
-    for year in tierras:
-        values = data2["Instituciones"]["Tenientes de tierras"][year][j]  #Luego "Por Provincias"
-        for key in values:
-            if key == "Cooperativas" and (isinstance(values[key], dict)):
-                for k in values[key]:
-                    data["features"][i]["properties"]["Tierras"+year+k] = values[key][k]   #Luego "Entidades"
-            else:
-                data["features"][i]["properties"]["Tierras"+year+key] = values[key]   #Luego "Entidades"
-
-
+n = len(list(df[0].index))
+for year in years:
+    for i in range(n):
+        values = list(df[years.index(year)].iloc[i])[1:]
+        for prov in lista_prov:
+            index = lista_prov.index(prov)
+            for k, j in list(zip(provincias,values)):
+                if j == "-":
+                    data["features"][index]["properties"][f"{k}{year}"] = j    
 try:
-    with open("data/geojsons/cuba.geojson", 'w') as archivo_json:
+    with open("app/data/geojsons/cuba.geojson", 'w') as archivo_json:
         json.dump(data, archivo_json, indent=4)
         print("GeoJson Actualizado de forma exitosa")
 except Exception as e:
